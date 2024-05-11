0fb28e258b30565a5a039580ade3f2c318d0553c
wonrax <hahuylonghai2012@gmail.com>
2024-05-11 15:56:32 +0700

    neovim: recolor todo-comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -179,7 +179,33 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      highlight = {
+        before = '',
+        keyword = 'fg',
+        after = '',
+      },
+
+      -- Test colors:
+      -- NOTE: note
+      -- TODO: todo
+      -- WARN: warn
+      -- FIXME: fixme
+      colors = {
+        error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+        warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+        info = { 'DiagnosticInfo', '#2563EB' },
+        hint = { 'DiagnosticHint', '#10B981' },
+        default = { 'Identifier', '#7C3AED' },
+        test = { 'Identifier', '#FF00FF' },
+      },
+    },
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
