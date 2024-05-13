06ef8e258ecc7edf6f033afe935c402026f0b326
wonrax <hahuylonghai2012@gmail.com>
2024-05-13 10:33:14 +0700

    neovim: fix floating border not working with new rustaceanvim version

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -2,5 +2,14 @@ return {
   {
     -- More advanced LSP support for Rust
     'mrcjkb/rustaceanvim',
+    config = function()
+      vim.g.rustaceanvim = {
+        tools = {
+          float_win_config = {
+            border = 'rounded',
+          },
+        },
+      }
+    end,
   },
 }
