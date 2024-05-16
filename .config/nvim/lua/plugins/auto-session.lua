a443e62851046d7b95851586556cfcbdea79c038
wonrax <hahuylonghai2012@gmail.com>
2024-05-16 09:07:38 +0700

    change log level and startup message

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -3,7 +3,7 @@ return {
     'rmagatti/auto-session',
     config = function()
       require('auto-session').setup {
-        log_level = 'info',
+        log_level = vim.log.levels.WARN,
         auto_session_enable_last_session = true,
         auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
         auto_session_enabled = true,
