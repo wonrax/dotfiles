d132908ed7669d39b436ed9ddbf345991819173d
wonrax <hahuylonghai2012@gmail.com>
2024-05-30 09:18:14 +0700

    neovim: add smart-open plugin for better frecency file picker

---
+++ .config/nvim/lua/plugins/smart-open.lua
@@ -0,0 +1,20 @@
+-- A better telescope-frecency.nvim
+return {
+  {
+    'danielfalk/smart-open.nvim',
+    branch = '0.2.x',
+    config = function()
+      require('telescope').load_extension 'smart_open'
+      vim.keymap.set('n', '<leader><leader>', function()
+        require('telescope').extensions.smart_open.smart_open {
+          cwd_only = true,
+        }
+      end, { noremap = true, silent = true, desc = 'Pick recent files or buffers' })
+    end,
+    dependencies = {
+      'kkharji/sqlite.lua',
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { 'nvim-telescope/telescope-fzy-native.nvim' },
+    },
+  },
+}
