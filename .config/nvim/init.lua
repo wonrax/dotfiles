345e923e48a30f76bf5fd59e1c5aed62c0118fe7
wonrax <hahuylonghai2012@gmail.com>
2024-06-01 13:45:51 +0700

    tmux: change switch pane keybinding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,9 +13,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Merge statusline and cmdline
-vim.opt.cmdheight = 0
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
@@ -33,6 +30,9 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+
+-- Merge statusline and cmdline
+vim.opt.cmdheight = 0
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
