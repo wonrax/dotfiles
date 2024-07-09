af5c803924b54ffce73948e5fdd47096f9e9d17b
wonrax <hahuylonghai2012@gmail.com>
2024-07-09 15:18:04 +0700

    neovim: conform now formats js and ts using prettier

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -27,6 +27,8 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         astro = { 'prettier' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
