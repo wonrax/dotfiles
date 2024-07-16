9e0182b38be56aa3b176809aee6a020b4eca4f23
wonrax <hahuylonghai2012@gmail.com>
2024-07-16 21:45:14 +0700

    neovim: fix deprecated which-key configs

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -4,34 +4,31 @@ return {
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup {
-        window = {
-          border = 'double',
-        },
+        preset = 'helix',
       }
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>l'] = { name = '[L]sp', _ = 'which_key_ignore' },
-        ['<leader>q'] = { name = '[Q]uit', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>*'] = { name = 'Switch to buffer [number]', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>*', group = 'Switch to buffer [number]' },
+        { '<leader>f', group = '[F]ind' },
+        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>l', group = '[L]sp' },
+        { '<leader>q', group = '[Q]uit' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>t', group = '[T]oggle/[T]abpage/[T]erminal' },
       }
 
       -- Hide <leader>[number] keybinds
       for i = 1, 9 do
-        require('which-key').register({
-          ['<leader>' .. i] = 'which_key_ignore',
-        }, { mode = 'n' })
+        require('which-key').add {
+          { '<leader>' .. i, hidden = true },
+        }
       end
 
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add {
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+      }
     end,
   },
 }
