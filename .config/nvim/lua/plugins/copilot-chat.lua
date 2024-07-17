34d6c41a91ea769e39d3dec3dae6f61e933f7eae
wonrax <hahuylonghai2012@gmail.com>
2024-07-17 16:59:10 +0700

    neovim: streamline prompts and descriptions in CopilotChat plugin
    This commit refactors the CopilotChat plugin in Neovim. The changes
    include streamlining the prompts and descriptions for better clarity
    and readability. The commit also removes some redundant code and
    updates the commit and commitStaged prompts. The changes aim to
    improve the user experience and maintainability of the code.

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -3,21 +3,13 @@
 
 local prompts = {
   -- Code related prompts
-  Explain = 'Explain how the following code works.',
-  Review = 'Review the following code and provide suggestions for improvement.',
-  Tests = 'Explain how the selected code works, then generate unit tests for it.',
-  Refactor = 'Refactor the following code to improve its clarity and readability.',
-  FixCode = 'Fix the following code to make it work as intended.',
-  FixError = 'Explain the error in the following text and provide a solution.',
-  BetterNamings = 'Provide better names for the following variables and functions.',
-  Documentation = 'Provide documentation for the following code.',
-  SwaggerApiDocs = 'Provide documentation for the following API using Swagger.',
-  SwaggerJsDocs = 'Write JSDoc for the following API using Swagger.',
+  Refactor = '/COPILOT_GENERATE Refactor the following code to improve its clarity, readability and maintainability.',
+  BetterNamings = '/COPILOT_REVIEW Provide better names for the following variables or functions.',
   -- Text related prompts
   Summarize = 'Summarize the following text.',
-  Spelling = 'Correct any grammar and spelling errors in the following text.',
-  Wording = 'Improve the grammar and wording of the following text.',
-  Concise = 'Rewrite the following text to make it more concise.',
+  Spelling = '/COPILOT_GENERATE Correct any grammar and spelling errors in the following text.',
+  Wording = '/COPILOT_GENERATE Improve the grammar and wording of the following text.',
+  Concise = '/COPILOT_GENERATE Rewrite the following text to make it more concise.',
 }
 
 return {
@@ -84,7 +76,7 @@ return {
           local actions = require 'CopilotChat.actions'
           require('CopilotChat.integrations.telescope').pick(actions.help_actions())
         end,
-        desc = 'CopilotChat - Help actions',
+        desc = 'Help actions',
       },
       -- Show prompts actions with telescope
       {
@@ -93,32 +85,32 @@ return {
           local actions = require 'CopilotChat.actions'
           require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
         end,
-        desc = 'CopilotChat - Prompt actions',
+        desc = 'Prompt actions',
       },
       {
         '<leader>cp',
         ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
         mode = 'x',
-        desc = 'CopilotChat - Prompt actions',
+        desc = 'Prompt actions',
       },
       -- Code related commands
-      { '<leader>ce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code', mode = { 'n', 'v' } },
-      { '<leader>ct', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests', mode = { 'n', 'v' } },
-      { '<leader>cr', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code', mode = { 'n', 'v' } },
-      { '<leader>cR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code', mode = { 'n', 'v' } },
-      { '<leader>cn', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Naming', mode = { 'n', 'v' } },
+      { '<leader>ce', '<cmd>CopilotChatExplain<cr>', desc = 'Explain code', mode = { 'n', 'v' } },
+      { '<leader>ct', '<cmd>CopilotChatTests<cr>', desc = 'Generate tests', mode = { 'n', 'v' } },
+      { '<leader>cr', '<cmd>CopilotChatReview<cr>', desc = 'Review code', mode = { 'n', 'v' } },
+      { '<leader>cR', '<cmd>CopilotChatRefactor<cr>', desc = 'Refactor code', mode = { 'n', 'v' } },
+      { '<leader>cn', '<cmd>CopilotChatBetterNamings<cr>', desc = 'Better Naming', mode = { 'n', 'v' } },
       -- Chat with Copilot in visual mode
       {
         '<leader>cv',
         ':CopilotChatVisual',
         mode = 'x',
-        desc = 'CopilotChat - Open in vertical split',
+        desc = 'Copilot Chat with selected text',
       },
       {
         '<leader>cx',
         ':CopilotChatInline<cr>',
         mode = 'x',
-        desc = 'CopilotChat - Inline chat',
+        desc = 'Inline chat',
       },
       -- Custom input for CopilotChat
       {
@@ -129,18 +121,18 @@ return {
             vim.cmd('CopilotChat ' .. input)
           end
         end,
-        desc = 'CopilotChat - Ask input',
+        desc = 'Ask input',
       },
       -- Generate commit message based on the git diff
       {
         '<leader>cm',
         '<cmd>CopilotChatCommit<cr>',
-        desc = 'CopilotChat - Generate commit message for all changes',
+        desc = 'Generate commit message for all changes',
       },
       {
         '<leader>cM',
         '<cmd>CopilotChatCommitStaged<cr>',
-        desc = 'CopilotChat - Generate commit message for staged changes',
+        desc = 'Generate commit message for staged changes',
       },
       -- Quick chat with Copilot
       {
@@ -151,32 +143,20 @@ return {
             vim.cmd('CopilotChatBuffer ' .. input)
           end
         end,
-        desc = 'CopilotChat - Quick chat',
+        desc = 'Quick chat',
       },
       -- Debug
-      { '<leader>cd', '<cmd>CopilotChatDebugInfo<cr>', desc = 'CopilotChat - Debug Info' },
+      { '<leader>cd', '<cmd>CopilotChatDebugInfo<cr>', desc = 'Debug Info' },
       -- Fix the issue with diagnostic
-      { '<leader>cf', '<cmd>CopilotChatFixDiagnostic<cr>', desc = 'CopilotChat - Fix Diagnostic' },
+      { '<leader>cf', '<cmd>CopilotChatFixDiagnostic<cr>', desc = 'Fix Diagnostic' },
       -- Clear buffer and chat history
-      { '<leader>cl', '<cmd>CopilotChatReset<cr>', desc = 'CopilotChat - Clear buffer and chat history' },
+      { '<leader>cl', '<cmd>CopilotChatReset<cr>', desc = 'Clear buffer and chat history' },
       -- Toggle Copilot Chat Vsplit
-      { '<leader>cv', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle' },
+      { '<leader>cv', '<cmd>CopilotChatToggle<cr>', desc = 'Toggle' },
     },
     config = function(_, opts)
       local chat = require 'CopilotChat'
       local select = require 'CopilotChat.select'
-
-      -- Override the git prompts message
-      opts.prompts.Commit = {
-        prompt = 'Write commit message for the change with commitizen convention',
-        selection = select.gitdiff,
-      }
-      opts.prompts.CommitStaged = {
-        prompt = 'Write commit message for the change with commitizen convention',
-        selection = function(source)
-          return select.gitdiff(source, true)
-        end,
-      }
 
       chat.setup(opts)
       -- Setup the CMP integration
