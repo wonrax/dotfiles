e222ed5c1eebf8090f84afce9002ec7e1d5d5aae
wonrax <hahuylonghai2012@gmail.com>
2024-07-26 11:19:00 +0700

    neovim: add pictograms to completion menu

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -34,6 +34,7 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'onsails/lspkind.nvim',
     },
     config = function()
       -- See `:help cmp`
@@ -108,6 +109,25 @@ return {
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'copilot-chat' },
+        },
+        formatting = {
+          expandable_indicator = true,
+          fields = {
+            'abbr',
+            'kind',
+            'menu',
+          },
+          format = function(entry, vim_item)
+            local lspkind = require 'lspkind'
+            return lspkind.cmp_format {
+              mode = 'symbol_text', -- show only symbol annotations
+              maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+              -- can also be a function to dynamically calculate max width such as
+              -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+              ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+              show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+            }(entry, vim_item)
+          end,
         },
       }
     end,
