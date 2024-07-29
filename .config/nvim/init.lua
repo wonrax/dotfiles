4e87e33f477c6babb0d3c19120f522d7a8d77857
wonrax <hahuylonghai2012@gmail.com>
2024-07-29 17:41:23 +0700

    neovim: change fillchar vert separator to look more streamlined

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,7 +25,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
-vim.opt.fillchars = { vert = '▏' }
+-- TODO: why direct assignment doesn't work?
+-- i.e. vim.opt.fillchars.vert = '▕'
+vim.opt.fillchars = { vert = '▕' }
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
