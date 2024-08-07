79b4a13d83f54fc00f36de1f4a2055a2135323c0
wonrax <hahuylonghai2012@gmail.com>
2024-08-07 18:45:21 +0700

    neovim: conform now use LSP as fallback formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -13,15 +13,11 @@ return {
       },
     },
     opts = {
-      notify_on_error = true,
+      notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
         return {
           timeout_ms = 5000,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_format = 'fallback',
         }
       end,
       formatters_by_ft = {
@@ -30,6 +26,7 @@ return {
         javascript = { 'prettier' },
         typescript = { 'prettier' },
         json = { 'prettier' },
+        jsonc = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
