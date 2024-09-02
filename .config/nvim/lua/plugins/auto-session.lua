9e930b0f00fbf95ac6d521a2d68fda94f54b9c10
wonrax <hahuylonghai2012@gmail.com>
2024-09-02 17:55:02 +0700

    neovim: remove custom session code since auto-session is fixed

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -13,16 +13,24 @@ return {
       vim.cmd 'set sessionoptions+=localoptions'
 
       require('auto-session').setup {
-        log_level = vim.log.levels.ERROR,
-        auto_session_enable_last_session = false,
-        auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
-        auto_session_enabled = true,
-        auto_save_enabled = true,
-        auto_restore_enabled = false,
-        auto_session_suppress_dirs = nil,
-        auto_session_use_git_branch = nil,
-        -- the configs below are lua only
-        bypass_session_save_file_types = nil,
+        enabled = true, -- Enables/disables auto creating, saving and restoring
+        root_dir = vim.fn.stdpath 'data' .. '/sessions/', -- Root dir where sessions will be stored
+        auto_save = true, -- Enables/disables auto saving session on exit
+        auto_restore = true, -- Enables/disables auto restoring session on start
+        auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+        suppressed_dirs = nil, -- Suppress session restore/create in certain directories
+        allowed_dirs = nil, -- Allow session restore/create in certain directories
+        auto_restore_last_session = true, -- On startup, loads the last saved session if session for cwd does not exist
+        use_git_branch = false, -- Include git branch name in session name
+        lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
+        bypass_save_filetypes = nil, -- List of file types to bypass auto save when the only buffer open is one of the file types listed, useful to ignore dashboards
+        close_unsupported_windows = true, -- Close windows that aren't backed by normal file before autosaving a session
+        args_allow_single_directory = true, -- Follow normal sesion save/load logic if launched with a single directory as the only argument
+        args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. While you can just set this to true, you probably want to set it to a function that decides when to save a session when launched with file args. See documentation for more detail
+        continue_restore_on_error = true, -- Keep loading the session even if there's an error
+        cwd_change_handling = true, -- Follow cwd changes, saving a session before change and restoring after
+        log_level = vim.log.levels.ERROR, -- Sets the log level of the plugin (debug, info, warn, error).
+
         pre_save_cmds = {
           -- 'Neotree close'
           function()
@@ -43,64 +51,10 @@ return {
         },
       }
 
-      vim.api.nvim_create_autocmd('VimEnter', {
-        callback = function()
-          local args = vim.fn.argv()
-
-          -- if there are more than one argument, then we don't want to restore
-          -- the session
-          if #args > 1 then
-            return
-          end
-
-          if #args == 0 then
-            local latest_session = require('auto-session.lib').get_latest_session(require('auto-session').get_root_dir())
-            if latest_session then
-              require('auto-session').RestoreSession(latest_session, false)
-            else
-              vim.api.nvim_exec_autocmds('User', {
-                -- TODO: move the user events to shared global variables
-                pattern = 'AutoSession::NoSessionRestored',
-              })
-            end
-            return
-          end
-
-          local arg = args[1]
-
-          -- check if the arg is file or directory
-          local stat = vim.loop.fs_stat(arg)
-
-          if stat then
-            if stat.type == 'directory' then
-              -- expand to absolute path
-              arg = vim.fn.expand(vim.fn.fnamemodify(arg, ':p'))
-
-              -- Remove trailing slash if it exists
-              if arg:sub(-1) == '/' then
-                arg = arg:sub(1, -2)
-              end
-
-              vim.api.nvim_set_current_dir(arg)
-              if require('auto-session').session_exists_for_cwd() then
-                require('auto-session').RestoreSession(arg, false)
-                return
-              end
-            end
-          end
-
-          vim.api.nvim_exec_autocmds('User', {
-            -- TODO: move the user events to shared global variables
-            pattern = 'AutoSession::NoSessionRestored',
-          })
-        end,
-      })
-
-      -- currently auto save doesn't work so we need to save the session manually
-      vim.api.nvim_create_autocmd('VimLeavePre', {
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'AutoSession::PreSessionSave',
         callback = function()
           require('neo-tree.command').execute { action = 'close' }
-          require('auto-session').SaveSession(nil, false)
         end,
       })
 
