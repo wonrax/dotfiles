9c337f128402c02e706550ca9228867b986f889a
wonrax <hahuylonghai2012@gmail.com>
2024-09-15 15:28:58 +0700

    tmux: add hooks to zsh and neovim to change window name eagerly

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -30,6 +30,16 @@ vim.api.nvim_create_autocmd('BufRead', {
   end,
 })
 
+-- Automatic rename tmux window after launching neovim
+-- https://github.com/ofirgall/tmux-window-name?tab=readme-ov-file#automatic-rename-after-launching-neovim
+vim.api.nvim_create_autocmd({ 'VimEnter', 'VimLeave' }, {
+  callback = function()
+    if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+      vim.loop.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. '/tmux-window-name/scripts/rename_session_windows.py', {})
+    end
+  end,
+})
+
 --  This function gets run when an LSP attaches to a particular buffer.
 --    That is to say, every time a new file is opened that is associated with
 --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
