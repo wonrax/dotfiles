79abe5e26896a3b5bab4828c0cb817a3b1f0c2bc
wonrax2 <hahuylonghai2012@gmail.com>
2024-10-23 11:54:33 +0700

    neovim: rustaceanvim config now merges default settings

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -3,7 +3,7 @@ return {
     -- More advanced LSP support for Rust
     'mrcjkb/rustaceanvim',
     init = function()
-      vim.g.rustaceanvim = {
+      local defaults = {
         tools = {
           float_win_config = {
             border = 'rounded',
@@ -13,10 +13,16 @@ return {
           default_settings = {
             ['rust-analyzer'] = {
               files = { excludeDirs = { 'node_modules' } },
+              check = {
+                command = 'clippy',
+              },
             },
           },
         },
       }
+
+      -- merge with existing vim.g.rustaceanvim
+      vim.g.rustaceanvim = vim.tbl_deep_extend('force', defaults, vim.g.rustaceanvim or {})
     end,
   },
 }
