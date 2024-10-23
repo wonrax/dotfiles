becc439516aecb94e1024e8ab89aebe0170e8c1c
wonrax2 <hahuylonghai2012@gmail.com>
2024-10-23 11:52:36 +0700

    neovim: enable exrc for per-project config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.opt.exrc = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
