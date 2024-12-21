da38646af5dc0b497601330b3fbca793aed3bf23
wonrax <hahuylonghai2012@gmail.com>
2024-12-21 13:29:40 +0700

    neovim: add hooks to change tmux window name

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,6 +109,7 @@ require 'keymaps'
 
 -- [[ Autocommands ]]
 require 'autocmds'
+require 'tmux'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
