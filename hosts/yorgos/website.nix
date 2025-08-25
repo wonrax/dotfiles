433e20a00fa68c3534512a4b10f8d9e4d4f6cd4c
wonrax <hahuylonghai2012@gmail.com>
2025-08-25 14:28:51 +0700

    nix: remove open-webui remnant in website.nix

--- hosts/yorgos/website.nix
+++ hosts/yorgos/website.nix
@@ -11,61 +11,6 @@
     "d /etc/wrx-sh/www/files.wrx.sh 0775 root root -"
   ];
 
-  virtualisation.oci-containers.containers."open-webui" = {
-    image = "ghcr.io/open-webui/open-webui:main";
-    environmentFiles = [
-      "/etc/open-webui/.env"
-    ];
-    environment = {
-      ENV = "prod";
-      WEBUI_AUTH = "true";
-      ENABLE_SIGNUP = "true";
-      ENABLE_LOGIN_FORM = "true";
-      DEFAULT_USER_ROLE = "pending";
-      ENABLE_MESSAGE_RATING = "false";
-      DATABASE_POOL_SIZE = "10";
-      ENABLE_OLLAMA_API = "false";
-      # The two options below helps significantly reducing RAM usage (from
-      # 600MB to 240MB)
-      # https://docs.openwebui.com/tutorials/tips/reduce-ram-usage
-      AUDIO_STT_ENGINE = "openai";
-      RAG_EMBEDDING_ENGINE = "ollama";
-    };
-    ports = [
-      "8080:8080"
-    ];
-    volumes = [
-      "/var/open-webui/data:/app/backend/data:rw"
-    ];
-    log-driver = "journald";
-    extraOptions = [
-      "--network=wrx-sh_default"
-    ];
-    labels = {
-      "io.containers.autoupdate" = "registry";
-    };
-  };
-
-  systemd.services."podman-open-webui" = {
-    serviceConfig = {
-      Restart = lib.mkOverride 90 "always";
-    };
-    after = [
-      "podman-network-wrx-sh_default.service"
-      "postgresql.service"
-    ];
-    requires = [
-      "podman-network-wrx-sh_default.service"
-      "postgresql.service"
-    ];
-    partOf = [
-      "podman-compose-wrx-sh-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-wrx-sh-root.target"
-    ];
-  };
-
   virtualisation.oci-containers.containers."schema-migrator" = {
     image = "ghcr.io/wonrax/wrx-sh-migrator:latest";
     environmentFiles = [
