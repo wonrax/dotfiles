2393ecfcd90f9e160399af8b88ebfca50cf5adae
wonrax <hahuylonghai2012@gmail.com>
2026-01-19 20:47:23 +0700

    nixos: disable nvidia open modules and power management
    Disabled nvidia.open and powerManagement.enable on peggy due to screen
    flickering issues when changing monitor refresh rates.

--- hosts/peggy/configuration.nix
+++ hosts/peggy/configuration.nix
@@ -105,7 +105,9 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = true;
+    # Disabled this because somehow it causes screen flickering when changing
+    # the refresh rate from 60hz to 75hz on my monitor.
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -118,7 +120,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
