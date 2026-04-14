53a7c670aa03f901645e50aa0ad11788e0789bc1
wonrax <hahuylonghai2012@gmail.com>
2026-04-14 23:52:21 +0700

    nix: pin caddy-l4 to 0.1.0

--- hosts/yorgos/caddy.nix
+++ hosts/yorgos/caddy.nix
@@ -4,8 +4,8 @@
     enable = true;
     configFile = ./Caddyfile;
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/mholt/caddy-l4@v0.0.0-20260216070754-eca560d759c9" ];
-      hash = "sha256-HhI0s8bi+T89dz0V0yfrTU/1NTK5wJUtxxn7Sg9Fi9g=";
+      plugins = [ "github.com/mholt/caddy-l4@v0.1.0" ];
+      hash = "sha256-/mxKD8218/cNlqfdrOuGCXsikqHN+FZIwB1rNinMIn0=";
     };
   };
 
